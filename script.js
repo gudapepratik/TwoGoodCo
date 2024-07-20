60d5e4e75f2aec713a0976c765e2ca11ddf538c5
Pratik Gudape <pratik.gudape23@pccoepune.org>
2024-07-20 19:24:54 +0530

    features added

--- script.js
+++ script.js
@@ -50,3 +50,47 @@ imageScale();
 
 // const scroll = new LocomotiveScroll();
 
+function heroPageAnimation() {
+    let tl = gsap.timeline();
+tl.from("#title", {
+    y: -30,
+    duration: 0.4,
+    opacity: 0,
+})
+
+tl.from(".nav-r-items", {
+    y: -10,
+    duration: 0.4,
+    opacity: 0,
+    stagger: 0.2,
+})
+tl.from(".hero-title", {
+    y: 50,
+    stagger: 0.1,
+    duration: 0.3,
+    opacity: 0,
+})
+
+}
+heroPageAnimation();
+
+function menuPage() {
+    
+const openMenuButton = document.querySelector("#open-menu");
+const closeMenuButton = document.querySelector("#close-menu");
+const menuContainer = document.querySelector("#menu-container");
+
+openMenuButton.addEventListener("click", () => {
+    menuContainer.classList.remove("hidden");
+    gsap.from(menuContainer,{
+        y: -1000,
+        duration:0.8,
+        ease: "expo.out",
+    })
+})
+
+closeMenuButton.addEventListener("click", () => {
+    menuContainer.classList.add("hidden");
+})
+}
+menuPage();
